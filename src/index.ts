351c55a3da9426cec76803ea7f07230853aba2ef
Paul Stone <stone@MacBook-Pro.domain>
2021-07-18 18:11:21 -0600

    configuring express app to use EJS

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import express from 'express'
+import path from "path"
 import router from './routes/github'
+import bodyParser from 'body-parser'
 
 const app = express()
 const port = 8080
+
+// Configure Express to use EJS
+app.set( "view engine", "ejs" );
+app.set( "views", path.join( __dirname, "views" ) );
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(router)
 
