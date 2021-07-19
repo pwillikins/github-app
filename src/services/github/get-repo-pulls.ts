fa8e0f30dedbe9734e8e0885926c4d026c07834d
Paul Stone <stone@MacBook-Pro.domain>
2021-07-18 19:20:57 -0600

    adding interface for getRepoPulls response

--- src/services/github/get-repo-pulls.ts
+++ src/services/github/get-repo-pulls.ts
@@ -1,11 +1,23 @@
 import axios from 'axios'
 
-export const getRepoPulls = async (url: string) => {
+interface PullsResponse {
+  pulls: Array<any>,
+  username: string,
+  project: string
+}
+
+export const getRepoPulls = async (url: string): Promise<PullsResponse> => {
   const [username, project] = getDetailsFromUrl(url)
 
   const reqUrl = `https://api.github.com/repos/${username}/${project}/pulls`
 
-  return await axios.get(reqUrl)
+  const response = await axios.get(reqUrl)
+
+  return {
+    pulls: response.data,
+    username: username,
+    project: project
+  }
 }
 
 const getDetailsFromUrl = (url: string) => {
