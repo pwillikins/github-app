c8f30f03cdf2e9ea13cc3da51b52b746d6c6ed39
Paul Stone <stone@MacBook-Pro.domain>
2021-07-18 18:14:17 -0600

    updating util method to include the view in the response

--- src/utils/response-handler.ts
+++ src/utils/response-handler.ts
@@ -1,7 +1,7 @@
 import express, { response } from 'express'
 
-export const handleSuccess = (res: express.Response, status: number, data: any) => {
-  return res.json({ data: data, status: status })
+export const handleSuccess = (res: express.Response, view: string, status: number, data: any) => {
+  return res.render(view, { data: data, status: status })
 }
 
 export const handleError = (res: express.Response, error: any) => {
